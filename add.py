224f98b95f0b5ab92429bbaca01ae0ac0a5459ca
Aravindan R <85152278+RAravindDS@users.noreply.github.com>
2021-11-20 17:23:03 +0530

    Initial Commit
    # My First commit for Testing Purpose

---
+++ add.py
@@ -0,0 +1,17 @@
+
+a = 200
+b = 2
+c = a * b
+
+def summa(a):
+    print(a)
+
+    """ This is a normal file for testing """
+print(c)
+summa()
+summa()
+for i in range(3):
+    print('tis')
+    for j in range(i):
+        print(j)
+        

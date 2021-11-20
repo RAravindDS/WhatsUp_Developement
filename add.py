f31369eaee521ea9c1e77bac8adda0966a551728
AravindanR22052001 <85152278+AravindanR22052001@users.noreply.github.com>
2021-11-20 17:42:27 +0530

    printed new one

--- add.py
+++ add.py
@@ -2,14 +2,17 @@
 a = 200
 b = 2
 c = a * b
+d = a + b 
 
 def summa(a):
     print(a)
+    print(d)
 
     """ This is a normal file for testing """
 print(c)
 summa()
 summa()
+
 for i in range(3):
     print('tis')
     for j in range(i):
